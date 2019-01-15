7032d44152fed3b7a100c4905b0c3bee3d089921
web-wangle <18829291736@163.com>
2019-01-15 17:29:17 +0800

    19-01-15-company mt search 开发

--- components/public/header/searchbar.vue
+++ components/public/header/searchbar.vue
@@ -6,20 +6,14 @@
       </el-col>
       <el-col :span="15" class="center">
         <div class="wrapper">
-          <el-input placeholder="搜索商家或地点"></el-input>
+          <el-input v-model="search" placeholder="搜索商家或地点" @focus="focus" @blur="blur" @input="input"></el-input>
           <button class="el-button el-button--primary"><i class="el-icon-search"></i></button>
-          <dl class="hotPlace">
+          <dl class="hotPlace" v-show="isHotPlace">
             <dt>热门搜索</dt>
-            <dd>火锅</dd>
-            <dd>火锅</dd>
-            <dd>火锅</dd>
-            <dd>火锅</dd>
+            <dd v-for="(item,index) in hotPlace" :key="index">{{item}}</dd>
           </dl>
-          <dl class="searchList">
-            <dd>火锅</dd>
-            <dd>火锅</dd>
-            <dd>火锅</dd>
-            <dd>火锅</dd>
+          <dl class="searchList" v-show="isSearchList">
+            <dd v-for="(item,index) in searchList" :key="index">{{item}}</dd>
           </dl>
         </div>
         <p class="suggset">
@@ -57,8 +51,34 @@ export default {
   name: '',
   data() {
     return {
+      search: '',
+      isFocus: false,
+      hotPlace: ['火锅','火锅','火锅'],
+      searchList: ['美食','美食','美食'],
     };
   },
+  computed: {
+    isHotPlace: function(){
+      return this.isFocus && !this.search
+    },
+    isSearchList: function(){
+      return this.isFocus && this.search
+    },
+  },
+  methods: {
+    focus: function(){
+      this.isFocus = true;
+    },
+    blur: function(){
+      let self = this;
+      setTimeout(function(){
+        self.isFocus = false;
+      },200)
+    },
+    input: function(){
+      // console.log('wangle');
+    }
+  }
 };
 </script>
 
