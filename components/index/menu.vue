4865f9d0017fd97b3321063ffce9cee9a4ee33ba
web-wangle <18829291736@163.com>
2019-01-16 21:35:30 +0800

    19-01-16-company mt menu 开发1.1

--- components/index/menu.vue
+++ components/index/menu.vue
@@ -1,28 +1,66 @@
 <template>
   <div class="m-menu">
-    <dl class="nav">
+    <dl class="nav" @mouseleave="mouseleave">
       <dt>全部分类</dt>
-      <dd v-for="(item,index) in menu" :key="index">
+      <dd v-for="(item,index) in menu" :key="index" @mouseenter="mouseenter">
         <i :class="item.type"></i>{{item.name}}<span class="arrow"></span>
       </dd>
     </dl>
+    <div class="detail" v-if="kind">
+      <div v-for="(item,index) in curdetail.child" :key="index">
+        <h4>{{item.title}}</h4>
+        <span v-for="(x,idx) in item.child" :key="idx">{{x}}</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: '',
   data() {
     return {
+      kind: '',
       menu: [{
         type: 'food',
-        title: '美食'
+        name: '美食',
+        child: [{
+          title: '美食',
+          child: ['代金券','甜点饮品','火锅','自助餐','小吃快餐']
+        }]
       },{
         type: 'takeout',
-        title: '外卖'
+        name: '外卖',
+        child: [{
+          title: '外卖',
+          child: ['美团外卖']
+        }]
+      },{
+        type: 'hotel',
+        name: '酒店',
+        child: [{
+          title: '酒店星级',
+          child: ['经济型','舒适/三星','高档/四星','豪华/五星']
+        }]
       }]
     };
   },
+  computed: {
+    curdetail(){
+      return this.menu.filter((item) => {
+        return item.type === this.kind;
+        })[0]
+    }
+  },
+  methods: {
+    mouseleave(){
+      setTimeout(() => {
+        this.kind = ''
+      },150)
+    },
+    mouseenter(e){
+      this.kind = e.target.querySelector('i').className;
+    },
+  }
 };
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="page-index">
     <el-row>
       <el-col :span="5">
-        <emenu></emenu>
+        <emenu/>
       </el-col>
       <el-col :span="19">2</el-col>
     </el-row>
