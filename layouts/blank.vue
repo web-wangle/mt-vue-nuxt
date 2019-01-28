252e03c3ef7ccd2d1e18ca5400e2327cf369d2ff
web-wangle <18829291736@163.com>
2019-01-28 16:35:31 +0800

    19-01-28-company mt 登录注册页面组件添加

---
+++ layouts/blank.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="layout-blank">
+    <nuxt></nuxt>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
